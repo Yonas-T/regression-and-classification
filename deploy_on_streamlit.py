9dfb5bee1f7adcfa0e13e930c87fa8bef5b5427e
Yonas-T <yonaztad@gmail.com>
2025-12-10 10:37:45 +0300

    add regressor on streamlit function

--- deploy_on_streamlit.py
+++ deploy_on_streamlit.py
@@ -5,6 +5,8 @@ import joblib
 model = joblib.load('models/audio_emotion_classifier_*.pkl')
 scaler = joblib.load('models/feature_scaler.pkl')
 encoder = joblib.load('models/label_encoder.pkl')
+regressor = joblib.load('models/retail_demand_prediction_gradient_boosting.pkl')
+
 
 st.title('Audio Emotion Classifier')
 
@@ -12,7 +14,7 @@ uploaded_file = st.file_uploader('Upload Audio File', type=['wav'])
 
 if uploaded_file:
     prediction, probabilities = predict_emotion(
-        uploaded_file, model, scaler, encoder
+        uploaded_file, model, scaler, encoder,regressor
     )
     st.write(f'Predicted Emotion: {prediction}')
     st.bar_chart(probabilities)
